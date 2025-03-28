85b35e40f3f8f5b7595a3ebd508ebed854ace915
Danny4life <osiki.segun@gmail.com>
2025-03-28 12:17:18 +0100

    added files

--- src/main/java/com/osiki/integrationTesting/service/UserService.java
+++ src/main/java/com/osiki/integrationTesting/service/UserService.java
@@ -4,6 +4,7 @@ import com.osiki.integrationTesting.entity.User;
 import com.osiki.integrationTesting.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,9 +14,12 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+
     public User registerUser(String name, String email) {
         User user = new User(name, email);
-        return userRepository.save(user);
+        user = userRepository.save(user);
+        userRepository.flush(); // Force Hibernate to commit the transaction
+        return user;
     }
 
     public List<User> getAllUsers() {
--- src/test/java/com/osiki/integrationTesting/service/UserServiceTest.java
+++ src/test/java/com/osiki/integrationTesting/service/UserServiceTest.java
@@ -9,7 +9,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -17,6 +20,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 //import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 //import static org.junit.jupiter.api.Assertions.*;
+
 
 @ExtendWith(SpringExtension.class) // Enables Spring testing in JUnit 5
 @DataJpaTest // Loads database-related components for testing
@@ -38,9 +42,13 @@ class UserServiceTest {
     void testRegisterUser() {
         User user = userService.registerUser("John Doe", "john@example.com");
 
+        List<User> users = userRepository.findAll();
+
         // Verify the user was saved to DB -- this is where we do the integration between the user service class and the database
         assertThat(user).isNotNull();
         assertThat(user.getId()).isNotNull();
+        assertThat(users).hasSize(1);
+        assertThat(users.get(0).getName()).isEqualTo("John Doe");
         assertThat(user.getEmail()).isEqualTo("john@example.com");
     }
 
